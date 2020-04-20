2ef78816955e7822f2caaa12b477409a84041769
ChenJT-code <1902123612@qq.com>
2020-04-20 23:25:18 +0800

    封装路由模块

---
+++ src/views/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="box">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+ .box{
+     width: 100px;
+     height: 100px;
+     background-color: red;
+ }
+</style>
