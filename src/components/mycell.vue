04281ed6ab64275a68042980b01120142eb6bd35
ChenJT-code <1902123612@qq.com>
2020-05-23 14:01:34 +0800

    封装数据单元格cell

---
+++ src/components/mycell.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="cell">
+    <span>{{title}}</span>
+    <div>
+      <span>{{desc}}</span>
+      <span class="iconfont icon-jiantou"></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['title', 'desc']
+}
+</script>
+
+<style lang='less' scoped>
+   .cell{
+     display: flex;
+     justify-content: space-between;
+     padding: 20px;
+     align-items: center;
+     border-bottom: 1px solid #333;
+     >span{
+        color: #333;
+     }
+     >div{
+       font-size: 14px;
+       color: #999;
+     }
+   }
+</style>
