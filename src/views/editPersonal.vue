bccf55e4e3c9b5f9cad732b7c7e7697f50943468
ChenJT-code <1902123612@qq.com>
2020-05-27 02:28:23 +0800

    修改密码和性别完成

--- src/views/editPersonal.vue
+++ src/views/editPersonal.vue
@@ -12,18 +12,47 @@
       <!-- 修改昵称 -->
       <Mycell title="昵称" :desc="userobj.nickname" @click="nickshow=true;nickname=userobj.nickname"></Mycell>
       <van-dialog
-      v-model="nickshow"
-      title="标题"
-      show-cancel-button
-      :close-on-click-overlay="true"
-      @confirm= 'uodatenick'
+        v-model="nickshow"
+        title="修改昵称"
+        show-cancel-button
+        :close-on-click-overlay="true"
+        @confirm="updatenick"
       >
         <van-cell-group>
           <van-field required clearable v-model="nickname" label="昵称" placeholder="请输入昵称" />
         </van-cell-group>
       </van-dialog>
-      <Mycell title="密码" :desc="userobj.password" type="password"></Mycell>
-      <Mycell title="性别" :desc="userobj.gender===1?'男':'女'"></Mycell>
+      <!-- 修改密码 -->
+      <Mycell title="密码" :desc="userobj.password" type="password" @click="passshow=true;"></Mycell>
+      <van-dialog
+        v-model="passshow"
+        title="修改密码"
+        show-cancel-button
+        :close-on-click-overlay="true"
+        @confirm="updatepass"
+        :before-close="beforeClose"
+      >
+        <van-cell-group>
+          <van-field required label="原密码" clearable v-model="password" placeholder="请输入原密码" />
+          <!-- <van-field required label="新密码" placeholder="请输入新密码" ref="userpwd" /> -->
+          <van-field required label="新密码" clearable v-model="new_password" placeholder="请输入新密码" />
+        </van-cell-group>
+      </van-dialog>
+      <!-- 修改性别 -->
+      <Mycell title="性别" :desc="userobj.gender===1?'男':'女'" @click="gendershow=true;" :defaultIndex='userobj.gender'></Mycell>
+      <van-dialog
+        v-model="gendershow"
+        title="修改性别"
+        show-cancel-button
+        :close-on-click-overlay="true"
+        @confirm="updategender"
+      >
+<van-picker
+  :columns="['女','男']"
+  @change="onChange"
+  :default-index="userobj.gender"
+/>
+      </van-dialog>
     </div>
   </div>
 </template>
@@ -37,8 +66,13 @@ import { uploadFile } from '@/apis/upload.js'
 export default {
   data () {
     return {
+      genderindex: '',
+      new_password: '',
       nickshow: false,
+      passshow: false,
+      gendershow: false,
       nickname: '',
+      password: '',
       id: '',
       userobj: {}
     }
@@ -49,8 +83,60 @@ export default {
   },
 
   methods: {
+    onChange (picker, value, index) {
+      console.log(`当前值：${value}, 当前索引：${index}`)
+      // 因为gender在数据库中存储就是0和1，类似于索引
+      // 为了之后能够使用到这个索引，将索引存储到data中
+      this.genderindex = index
+    },
+    // 修改性别
+    async updategender () {
+      const res = await updateUserInfo(this.id, { gender: this.genderindex })
+      if (res.data.message === '修改成功') {
+        this.$toast.success('修改成功')
+        this.userobj.gender = this.genderindex
+      }
+    },
+    // 关闭弹窗前触发
+    // action:当前操作的类型：cancel  comfirm
+    // done:关闭时所执行的操作：done():关闭 done(false):不关闭
+    beforeClose (action, done) {
+      // 如果密码输入不匹配，则给出提示，并中止本次请求
+      // 只有单击确认的时候，才需要进行原密码是否匹配的验证
+      if (action === 'confirm' && this.password !== this.userobj.password) {
+        this.$toast.success('原密码输入不正确')
+        done(false)
+      } else {
+        done()
+      }
+    },
+    // 修改密码
+    // async updatepass () {
+    //   if (this.password === this.userobj.password) {
+    //     // console.log(this.$refs.userpwd.$refs.input.value)
+    //     const pwd = this.$refs.userpwd.$refs.input.value
+    //     const res = await updateUserInfo(this.id, { password: pwd })
+    //     if (res.data.message === '修改成功') {
+    //       this.$toast.success('修改成功')
+    //       this.userobj.password = pwd
+    //     }
+    //   }
+    // },
+    async updatepass () {
+      if (this.password === this.userobj.password) {
+        const res = await updateUserInfo(this.id, {
+          password: this.new_password
+        })
+        if (res.data.message === '修改成功') {
+          this.$toast.success('修改成功')
+          this.userobj.password = this.new_password
+          this.password = ''
+          this.new_password = ''
+        }
+      }
+    },
     // 修改昵称
-    async uodatenick () {
+    async updatenick () {
       const res = await updateUserInfo(this.id, { nickname: this.nickname })
       // console.log(res)
       if (res.data.message === '修改成功') {
